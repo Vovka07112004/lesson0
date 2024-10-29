d4a44dfcd68f6bb01f2c30181d7d845133e48b0a
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-10-29 14:46:05 +0300

    Variables
    Цель: научиться правильно называть переменные и взаимодействовать с ними.

---
+++ Variables.py
@@ -0,0 +1,6 @@
+#Цель: научиться правильно называть переменные и взаимодействовать с ними.
+homework = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (hours_spent / homework)
+print('Курс: ' + course_name + ', всего задач:' + str(homework) + ", затрачено часов:" + str(hours_spent) + ", среднее время выполнения " + str(time_for_one_task) + " часа.")
