5f2473e2ebdd06b1254574f3ecde35d88d5e1b34
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-10-29 11:52:36 +0300

    Динамическая типизация
    Цель: Написать программу на языке Python, используя Pycharm, для демонстрации динамической типизации.

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+#Цель: Написать программу на языке Python, используя Pycharm, для демонстрации динамической типизации.
+name = 'Vladimir'
+print(name)
+age = 19
+print(age)
+new_age = 20
+print(new_age)
+is_strue = True
+print(is_strue)
+
+print("Здравствуйте, меня зовут " + name + ". Мне " + str(age) + " лет, а не " + str(new_age) + ", и вся эта информация " + str(is_strue) + '!')
