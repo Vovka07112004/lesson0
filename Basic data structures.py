3b5c8700eb9d59a6ab104d8ac6236f9c6a4f5f01
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-11-03 07:46:24 +0300

    Организация программ и методы строк
    Цель: Написать программу на языке Python с использованием Pycharm для работы с методами строк и организации программ.

--- Basic data structures.py
+++ Basic data structures.py
@@ -1,7 +1,7 @@
 #Цель: Научиться работать со строками и индексацией строк в Python.
-example = 'Vladimir'
+example = "Vladimir"
 print("Первый символ:", example[0])
-print("Последний символ: " + example[-1])
-print("Вторая половина: "+ example[4::])
+print("Последний символ:" + example[-1])
+print("Вторая половина:",example[3:])
 print("Слово наоборот:", example[::-1])
 print("Каждый второй символ:", example[1:8:2])
