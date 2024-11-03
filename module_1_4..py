a272e3cc1774f7d284a9e71011b12e722172349c
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-11-03 07:47:40 +0300

    Организация программ и методы строк
    Цель: Написать программу на языке Python с использованием Pycharm для работы с методами строк и организации программ.

---
+++ module_1_4..py
@@ -0,0 +1,7 @@
+my_string = input("Введите любую строку: ")
+print("Количество символов в строке:", len(my_string))
+print("Верхний регистр:", my_string.upper())
+print("Нижний регистр:", my_string.lower())
+print("Строка без пробелов:", my_string.replace(" ", ""))
+print("Первый символ строки:", my_string[0])
+print("Последний символ строки:", my_string[-1])
