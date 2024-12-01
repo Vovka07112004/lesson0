1c280b893152e64634df82f06fd0e3c930e05123
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-12-01 13:25:49 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -2,7 +2,7 @@
 def generate_password(n):
     result = ""
     for i in range(1, n):
-        for j in range(i + 1, n + 1):
+        for j in range(i + 1, n):
             pair_sum = i + j
             if n % pair_sum == 0:
                 result += f"{i}{j}"
