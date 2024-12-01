55ff9f7a6869efb1e6d2f3e976746f2f91ea8e8c
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-12-01 13:15:56 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,12 +1,13 @@
 #Цель: закрепить навык решения задач при помощи цикла for, применив знания об основных типах данных.
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-numbers.remove(1)
 for number in numbers:
+    if number == 1:
+        continue
     is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
+    for divisor in range(2, number):
+        if number % divisor == 0:
             is_prime = False
             break
     if is_prime:
@@ -15,4 +16,5 @@ for number in numbers:
         not_primes.append(number)
 print("Primes:", primes)
 print("Not Primes:", not_primes)
+
 
