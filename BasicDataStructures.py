307a65c85fcd41157ec028b0e2c84e0dafd02881
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-12-01 13:11:26 +0300

    Add files via upload

--- BasicDataStructures.py
+++ BasicDataStructures.py
@@ -1,10 +1,11 @@
 #Цель: Применить знания полученные в модуле, решив задачу повышенного уровня сложности
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_list = list(students)
+sorted_students = sorted(list(students))
 average_grades = {}
-for i in range(len(grades)):
-    average_grade = sum(grades[i]) / len(grades[i])
-
-    average_grades[students_list[i]] = average_grade
+for i in range(len(sorted_students)):
+    student_name = sorted_students[i]
+    student_grades = grades[i]
+    average_grade = sum(student_grades) / len(student_grades)
+    average_grades[student_name] = average_grade
 print(average_grades)
