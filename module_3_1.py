d2507d914fb86c158e3460a1d2a0f312311e90aa
Vovka07112004 <entertainmentchannel.dr.99@gmail.com>
2024-12-01 13:31:09 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -3,21 +3,17 @@ calls = 0
 def count_calls():
     global calls
     calls += 1
-def string_info(name):
+def string_info(string):
     count_calls()
-    string = len(name), name.upper(), name.lower()
-    return string
-def is_contains(first_, second_):
+    length = len(string)
+    upper = string.upper()
+    lower = string.lower()
+    return (length, upper, lower)
+def is_contains(string, list_to_search):
     count_calls()
-    first1 = first_
-    first= first1.lower()
-    second3 = second_
-    second2 = str(second3)
-    second = second2.lower()
-    if first in second:
-        return True
-    else:
-        return False
+    string_lower = string.lower()
+    list_lower = [item.lower() for item in list_to_search]
+    return string_lower in list_lower
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
